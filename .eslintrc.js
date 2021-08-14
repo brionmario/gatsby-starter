bcf9d617458fc275a1c5395da61f99fcf37e6c41
Brion Mario <info@brionmario.com>
2021-08-14 15:22:33 +0530

    chore(base): rearrange eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,25 +17,55 @@
 
 module.exports = {
     env: {
-        jest: true,
-        node: true // defines things like process.env when generating through node,
+        browser: true, // Browser global variables like `window` etc.
+        commonjs: true, // CommonJS global variables and CommonJS scoping.Allows require, exports and module.
+        es6: true, // Enable all ECMAScript 6 features except for modules.
+        jest: true, // Jest global variables like `it` etc.
+        node: true // Defines things like process.env when generating through node
     },
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended",
         "plugin:jsx-a11y/recommended",
-        "plugin:react-hooks/recommended"
+        "plugin:react-hooks/recommended",
+        "plugin:jest/recommended",
+        "plugin:testing-library/react"
     ],
-    parser: "babel-eslint", // uses babel-eslint transforms
+    overrides: [
+        {
+            files: [ "**/*.ts?(x)" ],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                },
+                ecmaVersion: 2018,
+                sourceType: "module"
+            },
+            plugins: [
+                "@typescript-eslint"
+            ],
+            // You can add Typescript specific rules here.
+            // If you are adding the typescript variant of a rule which is there in the javascript
+            // ruleset, disable the JS one.
+            rules: {
+                "@typescript-eslint/no-array-constructor": "warn",
+                "no-array-constructor": "off"
+            }
+        }
+    ],
+    parser: "babel-eslint", // Uses babel-eslint transforms.
     parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: "module" // Allows for the use of imports
     },
     plugins: [
-        "import",
-        "@typescript-eslint"
+        "import" // eslint-plugin-import plugin. https://www.npmjs.com/package/eslint-plugin-import
     ],
+    root: true, // For configuration cascading.
     rules: {
         "comma-dangle": [
             "warn",
@@ -75,34 +105,6 @@ module.exports = {
         ],
         "no-console": "warn",
         "no-duplicate-imports": "warn",
-        "no-restricted-imports": [
-            "error",
-            {
-                paths: [
-                    {
-                        message: "Please use import foo from 'lodash-es/foo' instead.",
-                        name: "lodash"
-                    },
-                    {
-                        message: "Avoid using chain since it is non tree-shakable. Try out flow instead.",
-                        name: "lodash-es/chain"
-                    },
-                    {
-                        importNames: ["chain"],
-                        message: "Avoid using chain since it is non tree-shakable. Try out flow instead.",
-                        name: "lodash-es"
-                    },
-                    {
-                        message: "Please use import foo from 'lodash-es/foo' instead.",
-                        name: "lodash-es"
-                    }
-                ],
-                patterns: [
-                    "lodash/**",
-                    "lodash/fp/**"
-                ]
-            }
-        ],
         "no-unused-vars": "warn",
         "object-curly-spacing": [
             "warn",
@@ -174,7 +176,7 @@ module.exports = {
     },
     settings: {
         react: {
-            version: "detect" // detect react version
+            version: "detect" // Detect react version
         }
     }
 };
